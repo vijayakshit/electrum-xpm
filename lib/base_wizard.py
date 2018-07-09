58a62eb89f7de56c7df34bc9385a71fbb4ca14be
James <james@theapplicationist.com>
2018-07-09 23:33:19 +0200

    Remove 2FA Temporarily, Will re-implement in future)

--- lib/base_wizard.py
+++ lib/base_wizard.py
@@ -93,7 +93,7 @@ class BaseWizard(object):
         ])
         wallet_kinds = [
             ('standard',  _("Standard wallet")),
-            ('2fa', _("Wallet with two-factor authentication")),
+            #('2fa', _("Wallet with two-factor authentication")),
             ('multisig',  _("Multi-signature wallet")),
             ('imported',  _("Import Primecoin addresses or private keys")),
         ]
