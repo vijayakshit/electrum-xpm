ecf0cd2586f8bd501eba77b297ac791ceb86f833
James <james@theapplicationist.com>
2018-07-10 23:46:00 +0200

    Update crash hub URL as a precaution

--- gui/qt/exception_window.py
+++ gui/qt/exception_window.py
@@ -53,7 +53,7 @@ issue_template = """<h2>Traceback</h2>
   <li>Locale: {locale}</li>
 </ul>
 """
-report_server = "https://crashhub.electrum.org/crash"
+report_server = "https://crashhub.primecommunity.org/crash"
 
 
 class Exception_Window(QWidget, MessageBoxMixin):
